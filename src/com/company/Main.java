b9ff488e1ae7281f66ffd116669f8d41e157aacf
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-03-16 23:03:37 -0700

    [WIP]Added Main Menu test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	    RockPaperScissors game = new RockPaperScissors();
-	    game.startGame();
+	    game.startGameUp();
     }
 
-//    public void startGame() {
+//    public void startGameUp() {
 //        System.out.println(("Ja-Ken-Pon!!"));
 //    }
 }
--- src/test/StartGameTest.java
+++ src/test/StartGameTest.java
@@ -5,9 +5,17 @@ import static org.junit.Assert.*;
 
 public class StartGameTest {
     @Test
-    public void gameStart_successfulGameStart() {
+    public void gameStartUp_successfulGameStartUp() {
         String expected = "Welcome to Rock Paper Scissors!";
         String actual = startGame();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mainMenu_successfulMainMenu() {
+        String expected = "MAIN MENU\====\1. Type 'play' to play\2. Type 'history' to view your game history\Type 'quit' to stop playing";
+        String actual = mainMenu();
 
         assertEquals(expected, actual);
     }
