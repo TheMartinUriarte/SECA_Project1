e2988f130b2209d626e56178b9790e024cc7f1d2
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-03-18 12:11:50 -0700

    [WIP] Created StartGameUp class. Converted test class name to StartGameUpTest

--- src/test/StartGameTest.java
+++ src/test/StartGameTest.java
@@ -3,18 +3,18 @@ package test;
 import org.junit.test;
 import static org.junit.Assert.*;
 
-public class StartGameTest {
+public class StartGameUpTest {
     @Test
-    public void gameStartUp_successfulGameStartUp() {
+    public void startGameUp_successfulStartGameUp() {
         String expected = "Welcome to Rock Paper Scissors!";
-        String actual = startGame();
+        String actual = startGameUp();
 
         assertEquals(expected, actual);
     }
 
     @Test
     public void mainMenu_successfulMainMenu() {
-        String expected = "MAIN MENU\====\1. Type 'play' to play\2. Type 'history' to view your game history\Type 'quit' to stop playing";
+        String expected = "MAIN MENU\ ====\ 1. Type 'play' to play\ 2. Type 'history' to view your game history\Type 'quit' to stop playing";
         String actual = mainMenu();
 
         assertEquals(expected, actual);
